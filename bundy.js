c000ee97647139a0891e97fa3bb42f18dcb5e37a
Michał Dudek <michal@michaldudek.pl>
2013-08-20 22:35:09 +0200

    Registering the package in Bower.

--- bundy.js
+++ bundy.js
@@ -1,9 +1,9 @@
 var bundy = require('bundy');
 
-bundy.js('src/jquery.appendix.js', 'minified/jquery.appendix.min.js');
+bundy.js('src/jquery.appendix.js', 'jquery.appendix.min.js');
 bundy.js([
 	'src/jquery.appendix.js',
 	'src/jquery.appendix.prototypes.js'
-], 'minified/jquery.appendix.p.min.js');
+], 'jquery.appendix.proto.min.js');
 
 bundy.build();
